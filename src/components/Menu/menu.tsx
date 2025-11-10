72d896fc2aa624b9aae6dae066e41e916475bb77
that-kimani <that.kimani@gmail.com>
2025-11-10 09:08:16 +0300

    Menu issue fix

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu as MenuIcon, X as CloseIcon } from 'lucide-react';
-import './Menu.css';
+import './menu.css';
 
 const Menu: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
